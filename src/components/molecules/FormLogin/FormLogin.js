8be7c7d78a3352c6b12c14967ace0b1c6ad12e8e
Raquel.Magnani <quel.magnani88@gmail.com>
2020-06-25 14:57:42 -0300

    resolução de quinta

---
+++ src/components/molecules/FormLogin/FormLogin.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import "./formLogin.css"
+
+const FormLogin = ({onFinish}) => {
+    return(
+        <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Campo obrigatório, insira seu nome de usuário!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Usuario" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Campo obrigatório, insira sua senha!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Senha"
+        />
+      </Form.Item>
+      
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Entrar
+        </Button>
+        Ou <a href="">Cadastre-se</a>
+      </Form.Item>
+    </Form>
+    )
+}
+
+export default FormLogin;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,11 +4,24 @@ import LoginContent from "../../components/organisms/LoginContent/LoginContent"
 import "./login.css";
 
 class Login extends Component{
+constructor(){
+    super();
+
+    this.state={
+
+    }
+}
+
+onFinish = (value)=>{
+    console.log("Dados enviados",value)
+}
+
     render(){
         return(
             <GeneralTemplate>
                 <LoginContent 
                 titulo="Acesse sua conta"
+                onFinish={this.onFinish}
                 />
 
             </GeneralTemplate>
